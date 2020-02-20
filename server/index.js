5cadb1059bf6c03e4bafbe3af371712a66a1337e
administrator <admin@administrators-MacBook-Air.local>
2020-02-20 13:43:08 +0000

    fix

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, '../../build')));
 app.use(cors());
-app.use(router);
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
