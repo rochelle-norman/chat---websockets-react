7e2f86ebf1d1d2818bb9ef25858a9a66f4028547
Andrea Spadavecchia <spadavecchia.andrea@spes.uniud.it>
2020-02-20 15:47:36 +0000

    deploy (#1)
    * change nodemon to node

--- server/index.js
+++ server/index.js
@@ -2,13 +2,15 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
+const path = require('path');
+const router = express.Router();
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
+app.use(express.static(path.join(__dirname, '../build')));
 const io = socketio(server);
 
 app.use(cors());
@@ -18,11 +20,11 @@ io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if(error) return callback(error);
+    if (error) return callback(error);
 
     socket.join(user.room);
 
-    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
@@ -41,11 +43,15 @@ io.on('connect', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
 
-    if(user) {
+    if (user) {
       io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
-      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
   })
+});
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
