1dd30970e3f97f4f4f42e046e35ccc544a81bb37
administrator <admin@administrators-MacBook-Air.local>
2020-02-20 15:30:43 +0000

    boh

--- server/index.js
+++ server/index.js
@@ -3,19 +3,18 @@ const express = require('express');
 const socketio = require('socket.io');
 const cors = require('cors');
 const path = require('path');
+const router = express.Router();
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 
 const app = express();
 const server = http.createServer(app);
+app.use(express.static(path.join(__dirname, '../build')));
 const io = socketio(server);
 
 app.use(cors());
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
+app.use(router);
 
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
@@ -49,6 +48,10 @@ io.on('connect', (socket) => {
       io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
   })
+});
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
